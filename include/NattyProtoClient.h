0c08b3550ea88f07c2346e0a6c2d0a8801a1e408
wangbojing <1989wangbojing@163.com>
2017-03-16 15:36:03 +0800

    修改客户端重连的问题。

--- include/NattyProtoClient.h
+++ include/NattyProtoClient.h
@@ -86,7 +86,6 @@ U8* ntyGetRecvBuffer(void);
 void ntySetDevId(DEVID id);
 int ntyGetRecvBufferSize(void);
 void ntyReleaseNetwork(void);
-int ntyGetNetworkStatus(void);
 void ntySetBindResult(PROXY_CALLBACK_CLINET cb);
 void ntySetUnBindResult(PROXY_CALLBACK_CLINET cb);
 void ntySetProxyDisconnect(PROXY_CALLBACK_CLINET cb);
