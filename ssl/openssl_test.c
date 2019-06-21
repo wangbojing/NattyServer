c13fc07726a2ab25aa0d20fe2c554bf22d463605
王博靖 <wangbojing@users.noreply.github.com>
2019-06-21 11:32:06 +0800

    Update openssl_test.c

--- ssl/openssl_test.c
+++ ssl/openssl_test.c
@@ -41,9 +41,42 @@
  *
  */
 
+//gcc -o openssl_test openssl_test.c openssl_aes_cfb.c openssl_aes_core.c openssl_cfb128.c -I ../include/
+
 #include <stdio.h>
+#include <string.h>
+#include "openssl_aes.h"
+
 
 int main(int argc, char **argv) {
-	
+
+	unsigned char ckey [] = "helloworldkey"; 
+ 	unsigned char ivec [] = "goodbyworldkey"; 
+ 
+ 	int bytes_read; 
+ 	unsigned char indata [AES_BLOCK_SIZE], outdata [AES_BLOCK_SIZE];; 
+ 	unsigned char decryptdata [AES_BLOCK_SIZE]; 
+ 
+ 	AES_KEY keyEn; 
+
+	AES_set_encrypt_key(ckey, 128, & keyEn);
+ 
+ 	int num = 0; 
+ 
+ 	strcpy((char *)indata, "Hello World");
+ 	bytes_read = sizeof(indata);
+ 
+ 	AES_cfb128_encrypt(indata, outdata, bytes_read, &keyEn, ivec, &num, AES_ENCRYPT);
+ 	printf("orig: %s\n", indata);
+
+	int i = 0;
+
+	for (i = 0;i < AES_BLOCK_SIZE;i ++) {
+		printf("outdata:%x\n", outdata[i]);
+	}
+ 	
+ 	return 0; 
+
 }
+
 
